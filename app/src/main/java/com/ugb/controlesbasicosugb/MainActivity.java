6d9687717eb87dbf17b0463061e5218b8c32b3a0
MINEDUCYT <SMTR072723@ugb.edu.sv>
2024-02-02 13:57:51 -0600

    Creacion de las pestañas del conversor

--- app/src/main/java/com/ugb/controlesbasicosugb/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicosugb/MainActivity.java
@@ -10,11 +10,20 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TabHost tbh;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tbh = findViewById(R.id.tbhConversor);
+        tbh.setup();
+
+        tbh.addTab(tbh.newTabspec( tag "LON")).setContent(R.id.Longitud).set.Indicador(label "LONGITUD", icon: null));
+        tbh.addTab(tbh.newTabspec( tag "ALM")).setContent(R.id.Almacenamiento).set.Indicador(label "ALMACENAMIENTO", icon: null));
+        tbh.addTab(tbh.newTabspec( tag "MON")).setContent(R.id.monedas).set.Indicador(label "MONEDAS", icon: null));
 
 
     }
