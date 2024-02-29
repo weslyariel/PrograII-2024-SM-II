42b22b7e154a87e26cc5f9f9ca7114f46fbba622
MINEDUCYT <SMTR072723@ugb.edu.sv>
2024-02-29 11:19:20 -0600

    Este es la creacion de la rama AgendaAmigosSqlite

--- app/src/main/java/com/ugb/controlesbasicosugb/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicosugb/MainActivity.java
@@ -15,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-            }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
 }
