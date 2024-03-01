f36d98b026343ee2d7894588f159c9e9e17b6dba
MINEDUCYT <SMTR072723@ugb.edu.sv>
2024-02-29 23:07:00 -0600

    Creacion de la Clase DB donde este hereda de la clase SQLiteOpenHelper y dichos metodos necesarios para la creación de la BD Y los datos

--- app/src/main/java/com/ugb/controlesbasicosugb/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicosugb/MainActivity.java
@@ -1,17 +1,10 @@
 package com.ugb.controlesbasicosugb;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
-import android.widget.TabHost;
-import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
