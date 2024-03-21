0faf2630628d21d284c97f5932ed370e36f1d425
MINEDUCYT <SMTR072723@ugb.edu.sv>
2024-03-21 01:27:06 -0600

    terminacion de BD Y activity_main.xml

--- app/src/main/java/com/ugb/controlesbasicosugb/DB.java
+++ app/src/main/java/com/ugb/controlesbasicosugb/DB.java
@@ -1,45 +1,49 @@
 package com.ugb.controlesbasicosugb;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DB extends SQLiteOpenHelper {
-    private static final String dbname="Amigos";
-    private static final int v=1;
-    private static final String SQLdb = "CREATE TABLE amigos (idAmigo integer primary key autoincrement, Nombre text, Dirección text, Telefono text," +
-            "Email text, Dui text)";
+import androidx.annotation.Nullable;
 
-    public DB(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+public class DB extends SQLiteOpenHelper {
+    private static final String dbname = "Amigos";
+    private static final int v = 1;
+    private static final String SQLdb = "CREATE TABLE amigos (idAmigo integer primary key autoincrement, Nombre text, Dirección text, Telefono text," +
+            "Email text, Dui text, Foto text)";
+
+    public DB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, dbname, factory, v);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(SQLdb);
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(SQLdb);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         //actualizar la estructura de la base de datos
     }
 
-    public String admininistrar_Amigos(String accion, String[] datos ){
+    public String admininistrar_Amigos(String accion, String[] datos) {
         try {
             SQLiteDatabase db = getWritableDatabase();
             String sql = "";
-            if (accion == "nuevo") {
-                sql = "INSERT INTO Amigos (Nombre, Direccion, Telefono, Email, Dui) Values ('" + datos[1] + "','" + datos[2] + "', '" + datos[3] + "', '" + datos[4] + "', '" + datos[5] + "')";
+            if (accion == ("nuevo")) {
+                sql = "INSERT INTO Amigos (Nombre, Direccion, Telefono, Email, Dui, Foto) Values ('" + datos[1] + "','" + datos[2] + "', '" + datos[3] + "', '" + datos[4] + "', '" + datos[5] + "', '" + datos[6] + "')";
             } else if (accion == "modificar") {
-                sql = "UPDATE Amigos Set Nombre='" + datos[1] + "',Direccion='" + datos[2] + "',Telefono='" + datos[3] + "',Email='" + datos[4] + "',Dui='" + datos[5] + "'WHERE IdAmigos'" + datos[0] + "'";
+                sql = "UPDATE Amigos Set Nombre='" + datos[1] + "',Direccion='" + datos[2] + "',Telefono='" + datos[3] + "',Email='" + datos[4] + "',Dui='" + datos[5], Foto = '" + datos[6] + "' D WHERE IdAmigos '" + datos[0] + "' ";
 
             } else if (accion == "eliminar") {
                 sql = "DELETE FROM Amigos WHERE IdAmigos'" + datos[0] + "'";
             }
             db.execSQL(sql);
             return "ok";
-        }catch (Exception e){
+        } catch (Exception e) {
             return e.getMessage();
         }
     }
 }
+
